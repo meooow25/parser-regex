0a3ff8f2bb1e4497e19e09372dd704d6c0fb1731
meooow25 <soumiksarkar.3120@gmail.com>
2024-03-07 08:57:42 +0530

    Make GenCaseFold a cabal script

--- script/case.sh
+++ script/case.sh
@@ -2,10 +2,12 @@
 set -eu
 
 echo "Downloading CaseFolding.txt..."
-curl --output script/script-data/CaseFolding.txt \
+curl --create-dirs \
+  --output script/script-data/CaseFolding.txt \
   https://www.unicode.org/Public/UCD/latest/ucd/CaseFolding.txt
 echo "Downloaded"
 echo "Generating hs..."
-cabal run script -- script/script-data/CaseFolding.txt \
+cabal run script/GenCaseFold.hs -- \
+  script/script-data/CaseFolding.txt \
   src/Regex/Internal/Generated/CaseFold.hs \
   Regex.Internal.Generated.CaseFold
